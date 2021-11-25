c5e3175163cf0c2846d687c1aabb6bf93fa2ed00
Tigran Najaryan <tnajaryan@splunk.com>
2021-11-25 12:19:30 -0500

    Add initial Server interface and types
    Contributes to https://github.com/open-telemetry/opamp-go/issues/16
    
    This is similar to what we did for the Client: it introduces an interface
    which can be implemented by the actual implementation later.

---
+++ server/types/callbacks.go
@@ -0,0 +1,50 @@
+package types
+
+import (
+	"net/http"
+
+	"github.com/open-telemetry/opamp-go/internal/protobufs"
+)
+
+// ConnectionResponse is the return type of the OnConnecting callback.
+type ConnectionResponse struct {
+	Accept             bool
+	HTTPStatusCode     int
+	HTTPResponseHeader map[string]string
+}
+
+type Callbacks interface {
+	// The following callbacks will never be called concurrently for the same
+	// connection. They may be called concurrently for different connections.
+
+	// OnConnecting is called when there is a new incoming connection.
+	// The handler can examine the request and either accept or reject the connection.
+	// To accept:
+	//   Return ConnectionResponse with Accept=true.
+	//   HTTPStatusCode and HTTPResponseHeader are ignored.
+	//
+	// To reject:
+	//   Return ConnectionResponse with Accept=false. HTTPStatusCode MUST be set to
+	//   non-zero value to indicate the rejection reason (typically 401, 429 or 503).
+	//   HTTPResponseHeader may be optionally set (e.g. "Retry-After: 30").
+	OnConnecting(request *http.Request) ConnectionResponse
+
+	// OnConnected is called when the WebSocket connection is successfully established
+	// after OnConnecting() returns and the HTTP connection is upgraded to WebSocket.
+	OnConnected(conn Connection)
+
+	// OnMessage is called when a message is received from the connection. Can happen
+	// only after OnConnected().
+	OnMessage(conn Connection, message *protobufs.AgentToServer)
+
+	// OnDisconnect is called when a Disconnect message is received from the client
+	// identified by instanceUid through the specified connection. Note that this is
+	// not the same as disconnecting the connection, the Disconnect is a message that
+	// the clients should send before the actual disconnection.
+	OnDisconnect(conn Connection, instanceUid string)
+
+	// OnConnectionClose is called when the WebSocket connection is closed.
+	// Typically, preceded by OnDisconnect() unless the client misbehaves or the
+	// connection is lost.
+	OnConnectionClose(conn Connection)
+}
