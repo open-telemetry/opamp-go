a39341e816fab493505617d30a14d78ff359b53e
Tigran Najaryan <4194920+tigrannajaryan@users.noreply.github.com>
2021-11-29 18:07:35 -0500

    Add initial Server interface and types (#21)
    Contributes to https://github.com/open-telemetry/opamp-go/issues/16
    
    This is similar to what we did for the Client: it introduces an interface
    which can be implemented by the actual implementation later.

---
+++ server/server.go
@@ -0,0 +1,49 @@
+package server
+
+import (
+	"context"
+	"crypto/tls"
+	"net/http"
+
+	"github.com/open-telemetry/opamp-go/server/types"
+)
+
+type Settings struct {
+	// Callbacks that the server will call after successful Attach/Start.
+	Callbacks types.Callbacks
+}
+
+type StartSettings struct {
+	Settings
+
+	// ListenEndpoint specifies the endpoint to listen on, e.g. "127.0.0.1:4320"
+	ListenEndpoint string
+
+	// Server's TLS configuration.
+	TLSConfig *tls.Config
+}
+
+type HTTPHandlerFunc func(http.ResponseWriter, *http.Request)
+
+type OpAMPServer interface {
+	// Attach prepares the OpAMP Server to begin handling requests from an existing
+	// http.Server. The returned HTTPHandlerFunc should be added as a handler to the
+	// desired http.Server by the caller and the http.Server should be started by
+	// the caller after that.
+	// For example:
+	//   handler, _ := server.Attach()
+	//   mux := http.NewServeMux()
+	//   mux.HandleFunc("/opamp", handler)
+	//   httpSrv := &http.Server{Handler:mux,Addr:"127.0.0.1:4320"}
+	//   httpSrv.ListenAndServe()
+	Attach(settings Settings) (HTTPHandlerFunc, error)
+
+	// Start an OpAMP Server and begin accepting connections. Starts its own http.Server
+	// using provided settings. This should block until the http.Server is ready to
+	// accept connections.
+	Start(settings StartSettings) error
+
+	// Stop accepting new connections and close all current connections. This should
+	// block until all connections are closed.
+	Stop(ctx context.Context) error
+}
---
+++ server/types/connection.go
@@ -0,0 +1,16 @@
+package types
+
+import (
+	"context"
+
+	"github.com/open-telemetry/opamp-go/internal/protobufs"
+)
+
+// Connection represents one OpAMP WebSocket connections.
+// The implementation MUST be a comparable type so that it can be used as a map key.
+type Connection interface {
+	// Send a message. Should not be called concurrently for the same Connection instance.
+	// Blocks until the message is sent.
+	// Should return as soon as possible if the ctx is cancelled.
+	Send(ctx context.Context, message *protobufs.ServerToAgent) error
+}
