1a4788059000252ac602cca6315f0eef443e65aa
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2022-08-18 01:53:47 +0530

    Implement gzip compression for HTTP client (#125)
    Resolves #112

--- client/httpclient_test.go
+++ client/httpclient_test.go
@@ -1,7 +1,10 @@
 package client
 
 import (
+	"compress/gzip"
 	"context"
+	"io/ioutil"
+	"net/http"
 	"sync/atomic"
 	"testing"
 	"time"
@@ -10,6 +13,7 @@ import (
 	"github.com/open-telemetry/opamp-go/client/types"
 	"github.com/open-telemetry/opamp-go/protobufs"
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/protobuf/proto"
 )
 
 func TestHTTPPolling(t *testing.T) {
@@ -45,6 +49,47 @@ func TestHTTPPolling(t *testing.T) {
 	srv.Close()
 
 	// Shutdown the client.
+	err := client.Stop(context.Background())
+	assert.NoError(t, err)
+}
+
+func TestHTTPClientCompression(t *testing.T) {
+	srv := internal.StartMockServer(t)
+	var reqCounter int64
+
+	srv.OnRequest = func(w http.ResponseWriter, r *http.Request) {
+		atomic.AddInt64(&reqCounter, 1)
+		assert.Equal(t, "gzip", r.Header.Get("Content-Encoding"))
+		reader, err := gzip.NewReader(r.Body)
+		assert.NoError(t, err)
+		body, err := ioutil.ReadAll(reader)
+		assert.NoError(t, err)
+		_ = r.Body.Close()
+		var response protobufs.AgentToServer
+		err = proto.Unmarshal(body, &response)
+		assert.NoError(t, err)
+		assert.Equal(t, response.AgentDescription.IdentifyingAttributes, []*protobufs.KeyValue{
+			{
+				Key:   "host.name",
+				Value: &protobufs.AnyValue{Value: &protobufs.AnyValue_StringValue{StringValue: "somehost"}},
+			}},
+		)
+		w.WriteHeader(http.StatusOK)
+	}
+
+	settings := types.StartSettings{EnableCompression: true}
+	settings.OpAMPServerURL = "http://" + srv.Endpoint
+	client := NewHTTP(nil)
+	prepareClient(t, &settings, client)
+
+	client.sender.SetPollingInterval(time.Millisecond * 10)
+
+	assert.NoError(t, client.Start(context.Background(), settings))
+
+	eventually(t, func() bool { return atomic.LoadInt64(&reqCounter) == 1 })
+
+	srv.Close()
+
 	err := client.Stop(context.Background())
 	assert.NoError(t, err)
 }
