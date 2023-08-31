d8914226b0d1612c3a04b345b3b2d37c9161af04
Phdevava <phdevava@cisco.com>
2023-08-31 07:55:31 -0700

    Added appropriate error message on failure

--- client/internal/packagessyncer.go
+++ client/internal/packagessyncer.go
@@ -278,7 +278,7 @@ func (s *packagesSyncer) downloadFile(ctx context.Context, pkgName string, file 
 
 	err = s.localState.UpdateContent(ctx, pkgName, resp.Body, file.ContentHash)
 	if err != nil {
-		return fmt.Errorf("cannot download file from %s: %v", file.DownloadUrl, err)
+		return fmt.Errorf("failed to install/update the package %s: %v", pkgName, err)
 	}
 	return nil
 }
