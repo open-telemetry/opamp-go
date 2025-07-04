a67cd120d66ec74858b2322ce633611e8f0d345d
Jacob Aronoff <jaronoff97@users.noreply.github.com>
2025-07-04 10:45:07 -0400

    Make it possible to set capabilities after client start (#366)
    Closes #365 
    
    Corresponds to spec change https://github.com/open-telemetry/opamp-spec/pull/217
    
    This PR introduces a new method on the client – `SetCapabilities`. This PR deprecates the former method of setting `Capabilities` via `StartSettings` in favor of calling `SetCapabilities` prior to start. In the future, the `Capabilities` field will be removed from `StartSettings` and `Start` will error if `SetCapabilities` was not called prior to `Start`. `SetCapabilities` will always ensure that at least `ReportsStatus` is present. Consumers of this method will now be able to change the Capabilities of the Client on demand. It is expected that servers are able to handle these changes.

--- client/internal/receivedprocessor.go
+++ client/internal/receivedprocessor.go
@@ -29,9 +29,6 @@ type receivedProcessor struct {
 	// packageSyncMutex protects against multiple package syncing operations at the same time.
 	packageSyncMutex *sync.Mutex
 
-	// Agent's capabilities defined at Start() time.
-	capabilities protobufs.AgentCapabilities
-
 	// Download reporter interval value
 	// a negative number indicates that the default should be used instead.
 	downloadReporterInt time.Duration
@@ -43,7 +40,6 @@ func newReceivedProcessor(
 	sender Sender,
 	clientSyncedState *ClientSyncedState,
 	packagesStateProvider types.PackagesStateProvider,
-	capabilities protobufs.AgentCapabilities,
 	packageSyncMutex *sync.Mutex,
 	downloadReporterInt time.Duration,
 ) receivedProcessor {
@@ -53,7 +49,6 @@ func newReceivedProcessor(
 		sender:                sender,
 		clientSyncedState:     clientSyncedState,
 		packagesStateProvider: packagesStateProvider,
-		capabilities:          capabilities,
 		packageSyncMutex:      packageSyncMutex,
 		downloadReporterInt:   downloadReporterInt,
 	}
@@ -185,7 +180,7 @@ func (r *receivedProcessor) ProcessReceivedMessage(ctx context.Context, msg *pro
 }
 
 func (r *receivedProcessor) hasCapability(capability protobufs.AgentCapabilities) bool {
-	return r.capabilities&capability != 0
+	return r.clientSyncedState.Capabilities()&capability != 0
 }
 
 func (r *receivedProcessor) rcvFlags(
@@ -204,6 +199,7 @@ func (r *receivedProcessor) rcvFlags(
 
 		r.sender.NextMessage().Update(
 			func(msg *protobufs.AgentToServer) {
+				msg.Capabilities = uint64(r.clientSyncedState.Capabilities())
 				msg.AgentDescription = r.clientSyncedState.AgentDescription()
 				msg.Health = r.clientSyncedState.Health()
 				msg.RemoteConfigStatus = r.clientSyncedState.RemoteConfigStatus()
