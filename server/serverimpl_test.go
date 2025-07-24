288a721343ab202d437e1889d151f28eb929b16f
Min-Uk.Lee <minuk.dev@gmail.com>
2025-07-24 10:02:58 +0900

    fix: fix wrong test of goleak.VerifyNone

--- server/serverimpl_test.go
+++ server/serverimpl_test.go
@@ -74,7 +74,7 @@ func TestServerStartStopWithCancel(t *testing.T) {
 }
 
 func TestServerStartStopCloseNoLeakGoroutine(t *testing.T) {
-	goleak.VerifyNone(t)
+	defer goleak.VerifyNone(t)
 	startSettings := &StartSettings{}
 	srv := startServer(t, startSettings)
 
