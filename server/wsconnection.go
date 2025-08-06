ccda4b559688277b828525410c27833affb6c5fe
Sam Hazlehurst <sam.hazlehurst@observiq.com>
2025-08-06 13:35:26 -0400

    fix: Add atomic.Bool to ensure wsConnection.Disconnect() is only called once (#359)
    Addresses this [issue](https://github.com/open-telemetry/opamp-go/issues/358) by addding an atomic.Bool so we only close the underlying websocket connection once. This PR also adds the test `TestDisconnectServerWSConnection` to ensure that that error log isn't generated when the connection is closed server-side. I renamed `TestDisconnectWSConnection` to `TestDisconnectClientWSConnection` since it tests closing the connection from the client-side, though the comments in the test implied it tested disconnecting from the server.
    
    Perhaps it would be better to simply ignore the error when we close the connection twice? I'm unsure if there are error conditions where we'd want to be able to try to close the connection again.
    
    Resolves https://github.com/open-telemetry/opamp-go/issues/358

--- server/wsconnection.go
+++ server/wsconnection.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net"
 	"sync"
+	"sync/atomic"
 
 	"github.com/gorilla/websocket"
 
@@ -17,23 +18,31 @@ type wsConnection struct {
 	// The websocket library does not allow multiple concurrent write operations,
 	// so ensure that we only have a single operation in progress at a time.
 	// For more: https://pkg.go.dev/github.com/gorilla/websocket#hdr-Concurrency
-	connMutex *sync.Mutex
+	connMutex sync.Mutex
 	wsConn    *websocket.Conn
+	closed    atomic.Bool
 }
 
 var _ types.Connection = (*wsConnection)(nil)
 
-func (c wsConnection) Connection() net.Conn {
+func newWSConnection(wsConn *websocket.Conn) types.Connection {
+	return &wsConnection{wsConn: wsConn}
+}
+
+func (c *wsConnection) Connection() net.Conn {
 	return c.wsConn.UnderlyingConn()
 }
 
-func (c wsConnection) Send(_ context.Context, message *protobufs.ServerToAgent) error {
+func (c *wsConnection) Send(_ context.Context, message *protobufs.ServerToAgent) error {
 	c.connMutex.Lock()
 	defer c.connMutex.Unlock()
 
 	return internal.WriteWSMessage(c.wsConn, message)
 }
 
-func (c wsConnection) Disconnect() error {
+func (c *wsConnection) Disconnect() error {
+	if !c.closed.CompareAndSwap(false, true) {
+		return nil
+	}
 	return c.wsConn.Close()
 }
