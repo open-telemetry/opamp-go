2b2d2c4d5e650de30f1cf7c6273097c9d0cab12d
Juande Manjon <Juande.Manjon@intusurg.com>
2025-11-12 22:43:37 -0800

    feat: add configurable TLS settings and server endpoint to agent

--- internal/examples/agent/agent/agent.go
+++ internal/examples/agent/agent/agent.go
@@ -23,7 +23,7 @@ import (
 
 	"github.com/open-telemetry/opamp-go/client"
 	"github.com/open-telemetry/opamp-go/client/types"
-	"github.com/open-telemetry/opamp-go/internal/examples/certs"
+	"github.com/open-telemetry/opamp-go/internal/examples/config"
 	"github.com/open-telemetry/opamp-go/protobufs"
 )
 
@@ -51,6 +51,8 @@ type Agent struct {
 
 	agentType    string
 	agentVersion string
+
+	agentConfig *config.AgentConfig
 
 	effectiveConfig string
 
@@ -87,12 +89,13 @@ func (p *proxySettings) Clone() *proxySettings {
 	}
 }
 
-func NewAgent(logger types.Logger, agentType string, agentVersion string, initialInsecureConnection bool) *Agent {
+func NewAgent(logger types.Logger, agentType, agentVersion string, agentConfig *config.AgentConfig) *Agent {
 	agent := &Agent{
 		effectiveConfig: localConfig,
 		logger:          logger,
 		agentType:       agentType,
 		agentVersion:    agentVersion,
+		agentConfig:     agentConfig,
 	}
 
 	agent.createAgentIdentity()
@@ -101,22 +104,13 @@ func NewAgent(logger types.Logger, agentType string, agentVersion string, initia
 
 	agent.loadLocalConfig()
 
-	if initialInsecureConnection {
-		agent.tlsConfig = &tls.Config{
-			InsecureSkipVerify: true,
-		}
-	} else {
-		tlsConfig, err := certs.CreateClientTLSConfig(
-			agent.opampClientCert,
-			certs.CaCert,
-		)
-		if err != nil {
-			agent.logger.Errorf(context.Background(), "Cannot load client TLS config: %v", err)
-			return nil
-		}
-		agent.tlsConfig = tlsConfig
+	tls, err := agentConfig.GetTLSConfig(context.Background())
+	if err != nil {
+		agent.logger.Errorf(context.Background(), "Cannot determine if TLS is required: %v", err)
+		return nil
 	}
-	if err := agent.connect(withTLSConfig(agent.tlsConfig)); err != nil {
+
+	if err = agent.connect(withTLSConfig(tls)); err != nil {
 		agent.logger.Errorf(context.Background(), "Cannot connect OpAMP client: %v", err)
 		return nil
 	}
@@ -148,7 +142,7 @@ func (agent *Agent) connect(ops ...settingsOp) error {
 	agent.opampClient = client.NewWebSocket(agent.logger)
 
 	settings := types.StartSettings{
-		OpAMPServerURL: "wss://127.0.0.1:4320/v1/opamp",
+		OpAMPServerURL: agent.agentConfig.Endpoint,
 		InstanceUid:    types.InstanceUid(agent.instanceId),
 		Callbacks: types.Callbacks{
 			OnConnect: func(ctx context.Context) {
---
+++ internal/examples/config/agent.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"net/url"
+
+	"github.com/open-telemetry/opamp-go/internal/examples/certs"
+	"go.opentelemetry.io/collector/config/configopaque"
+	"go.opentelemetry.io/collector/config/configtls"
+)
+
+type AgentConfig struct {
+	Endpoint   string                 `mapstructure:"endpoint"`
+	TLSSetting configtls.ClientConfig `mapstructure:"tls,omitempty"`
+}
+
+func (a *AgentConfig) GetTLSConfig(ctx context.Context) (*tls.Config, error) {
+	parsedURL, err := url.Parse(a.Endpoint)
+	if err != nil {
+		return nil, fmt.Errorf("parse server endpoint: %w", err)
+	}
+
+	if parsedURL.Scheme != "wss" && parsedURL.Scheme != "https" {
+		return nil, nil
+	}
+
+	// Default settings
+	if len(a.TLSSetting.CAFile) == 0 && len(a.TLSSetting.KeyFile) == 0 && len(a.TLSSetting.CertFile) == 0 {
+		a.TLSSetting.CAPem = configopaque.String(certs.CaCert)
+	}
+
+	return a.TLSSetting.LoadTLSConfig(ctx)
+}
