f9e370c9df8fd796979d0d4529a842c262e9548e
Michel Laterman <82832767+michel-laterman@users.noreply.github.com>
2026-01-09 13:09:30 -0600

    Server uses sync.Pool for gzip.Writer allocations (#484)
    Use of gzip.Writer pool for plain HTTP responses - gzip.Writers have a large memory footprint, if many agent clients clients were to respond at once a server could have an OOM crash. Use of the pool also reduces allocs.
    
    ```
    go test -v -benchmem -benchtime 5s -bench=BenchmarkCompressGzip
    ...
    goos: darwin
    goarch: arm64
    pkg: github.com/open-telemetry/opamp-go/server
    cpu: Apple M3 Pro
    BenchmarkCompressGzip
    BenchmarkCompressGzip/with_pool
    BenchmarkCompressGzip/with_pool-12         	  483432	     12155 ns/op	     113 B/op	       2 allocs/op
    BenchmarkCompressGzip/no_pool
    BenchmarkCompressGzip/no_pool-12           	   96456	     60972 ns/op	  813988 B/op	      19 allocs/op
    ```

--- server/serverimpl.go
+++ server/serverimpl.go
@@ -43,6 +43,10 @@ type server struct {
 	httpServer        *http.Server
 	httpServerServeWg *sync.WaitGroup
 
+	// gwPool is a gzip.Writer pool. This is intended to lower the amount of writers that are created when responding to HTTP requests.
+	// For best results HTTP responses should also have a rate-limit of some sort.
+	gwPool sync.Pool
+
 	// The network address Server is listening on. Nil if not started.
 	addr net.Addr
 }
@@ -65,7 +69,14 @@ func New(logger types.Logger) *server {
 		logger = &internal.NopLogger{}
 	}
 
-	return &server{logger: logger}
+	return &server{
+		logger: logger,
+		gwPool: sync.Pool{
+			New: func() any {
+				return gzip.NewWriter(io.Discard)
+			},
+		},
+	}
 }
 
 func (s *server) Attach(settings Settings) (HTTPHandlerFunc, ConnContext, error) {
@@ -336,9 +347,12 @@ func (s *server) readReqBody(req *http.Request) ([]byte, error) {
 	return data, nil
 }
 
-func compressGzip(data []byte) ([]byte, error) {
+func (s *server) compressGzip(data []byte) ([]byte, error) {
 	var buf bytes.Buffer
-	w := gzip.NewWriter(&buf)
+	w, _ := s.gwPool.Get().(*gzip.Writer)
+	defer s.gwPool.Put(w)
+	w.Reset(&buf)
+
 	_, err := w.Write(data)
 	if err != nil {
 		return nil, err
@@ -407,7 +421,7 @@ func (s *server) handlePlainHTTPRequest(req *http.Request, w http.ResponseWriter
 	// Send the response.
 	w.Header().Set(headerContentType, contentTypeProtobuf)
 	if req.Header.Get(headerAcceptEncoding) == contentEncodingGzip {
-		bodyBytes, err = compressGzip(bodyBytes)
+		bodyBytes, err = s.compressGzip(bodyBytes)
 		if err != nil {
 			s.logger.Errorf(req.Context(), "Cannot compress response: %v", err)
 			w.WriteHeader(http.StatusInternalServerError)
